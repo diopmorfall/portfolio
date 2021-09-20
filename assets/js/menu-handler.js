ee7eb9b29204cbfdd3c8d24a15ba3777f1a3e01e
diopmorfall <morfall.papydiop99@gmail.com>
2021-09-20 15:43:21 +0200

    Implementation of the mobile, tablet and desktop layouts, fixing of the project section

--- assets/js/menu-handler.js
+++ assets/js/menu-handler.js
@@ -1,7 +1,8 @@
-var menu = document.querySelector(".mobile-menu");
+var menu = document.querySelector(".menu");
 var openMenuIcon = document.querySelector(".menu-icons").children[0];
 var closeMenuIcon = document.querySelector(".menu-icons").children[1];
 var links = document.querySelectorAll(".menu-link");
+let mediaQueries = ["(min-width: 650px) and (max-width: 1000px)", "(min-width: 1000px)"];
 
 function closeMenu(){
 	menu.classList.add("menu-fade-out");
@@ -11,15 +12,17 @@ function closeMenu(){
 	openMenuIcon.style.display = 'block';
 }
 
-openMenuIcon.addEventListener('click', function(){
-	menu.classList.add("menu-fade-in");
-	menu.classList.remove("menu-fade-out");
-	menu.style.display = 'flex';
-	openMenuIcon.style.display = 'none';
-	closeMenuIcon.style.display = 'block';
-});
-
-closeMenuIcon.addEventListener('click', closeMenu);
-for(let i = 0; i < links.length; i++){
-	links[i].addEventListener('click', closeMenu);
-}
+//if((window.matchMedia(mediaQueries[0]).matches) && (!window.matchMedia(mediaQueries[1]).matches)){
+	openMenuIcon.addEventListener('click', function(){
+		menu.classList.add("menu-fade-in");
+		menu.classList.remove("menu-fade-out");
+		menu.style.display = 'flex';
+		openMenuIcon.style.display = 'none';
+		closeMenuIcon.style.display = 'block';
+	});
+	
+	closeMenuIcon.addEventListener('click', closeMenu);
+	for(let i = 0; i < links.length; i++){
+		links[i].addEventListener('click', closeMenu);
+	}
+//}
--- assets/js/projects-handler.js
+++ assets/js/projects-handler.js
@@ -12,19 +12,19 @@ const targetLinks = [
 let projectsArrays = [
 	[
 		"Sunnyside agency landing page",
-		"A perfect test about responsive layouts and images, with a bit of JavaScript for the mobile menu.<br>This was the most challenging project from Frontend Mentor I did so far, because I didn't knew how to handle responsive images with the srcset and sizes attributes of the img tag",
+		"A great test on responsive layouts and images, with a bit of JavaScript for the mobile menu.<br>Building this was tough, because I didn't know how to handle responsive images with <code>srcset<code> and <code>sizes</code> attributes",
 		"https://diopmorfall.github.io/sunnyside-agency-landing-page-main",
 		"https://github.com/diopmorfall/sunnyside-agency-landing-page-main"
 	],
 	[
 		"Four card feature section master", 
-		"A layout-based project from Frontend Mentor that'll challenge everyone who isn't familiar with responsive and multi-column layouts, just like me when I did it",
+		"A layout-based project from Frontend Mentor that'll challenge everyone who isn't familiar with responsive and multi-column layouts (like me when I did it)",
 		"https://diopmorfall.github.io/four-card-feature-section-master",
 		"https://github.com/diopmorfall/four-card-feature-section-master"
 	],
 	[
 		"Order Summary component",
-		"A small project for people who are getting started with basic layouts",
+		"A small project for people who are getting started with basic layouts, that I look forward to use in some bigger projects",
 		"https://diopmorfall.github.io/order-summary-component",
 		"https://github.com/diopmorfall/order-summary-component"
 	]
@@ -45,7 +45,7 @@ function showProjects(){
 	}
 
 	projects[index - 1].style.display = 'block';
-	//console.log(projects[j - 1].children[0].src);
+	//console.log(projects[index - 1].children[0].src);
 	setDetails(index -1);
 	timeout = setTimeout(showProjects, 15000);
 	//todo: fadein, show, fadeout, repeat with the next
